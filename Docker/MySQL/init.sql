fc0af3c074bd70694ff50e4d0405846857c08072
Fuji <jifu7833@gmail.com>
2024-08-04 22:09:37 +0900

    [add]init.sqlにテーブルの追加

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -1,34 +1,88 @@
 
-DROP DATABASE chatapp;
+DROP DATABASE chimyapp;
 DROP USER 'testuser';
 
 CREATE USER 'testuser' IDENTIFIED BY 'testuser';
-CREATE DATABASE chatapp;
+CREATE DATABASE chimyapp;
 USE chatapp
-GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
+GRANT ALL PRIVILEGES ON chimyapp.* TO 'testuser';
 
-CREATE TABLE users (
-    uid varchar(255) PRIMARY KEY,
-    user_name varchar(255) UNIQUE NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    password varchar(255) NOT NULL
+CREATE TABLE schools (
+    school_id INT AUTO_INCREMENT PRIMARY KEY,
+    school_name VARCHAR(100) NOT NULL,
+    parent_auth_key VARCHAR(100) NOT NULL,
+    teacher_auth_key VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE channels (
-    id serial PRIMARY KEY,
-    uid varchar(255) REFERENCES users(uid),
-    name varchar(255) UNIQUE NOT NULL,
-    abstract varchar(255)
+CREATE TABLE academic_levels (
+    academic_level_id INT AUTO_INCREMENT PRIMARY KEY,
+    grade VARCHAR(10) NOT NULL,
+    section VARCHAR(10) NOT NULL,
+    school_id INT,
+    FOREIGN KEY (school_id) REFERENCES schools(school_id)
+);
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name_kanji_full VARCHAR(100) NOT NULL,
+    name_kana_full VARCHAR(100) NOT NULL,
+    parent_name VARCHAR(100),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student', 'teacher') NOT NULL,
+    phone_number VARCHAR(15),
+    academic_level_id INT,
+    school_id INT,
+    FOREIGN KEY (academic_level_id) REFERENCES academic_levels(academic_level_id),
+    FOREIGN KEY (school_id) REFERENCES schools(school_id)
+);
+
+CREATE TABLE groups (
+    group_id INT AUTO_INCREMENT PRIMARY KEY,
+    group_name VARCHAR(100),
+    academic_level_id INT,
+    FOREIGN KEY (academic_level_id) REFERENCES academic_levels(academic_level_id)
+);
+
+CREATE TABLE user_groups (
+    user_group_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    group_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
 CREATE TABLE messages (
-    id serial PRIMARY KEY,
-    uid varchar(255) REFERENCES users(uid),
-    cid integer REFERENCES channels(id) ON DELETE CASCADE,
-    message text,
-    created_at timestamp not null default current_timestamp
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    message TEXT NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INT,
+    group_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
-INSERT INTO users(uid, user_name, email, password)VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
+CREATE TABLE notices (
+    notice_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    post_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES users(user_id)
+);
+
+
+INSERT INTO schools (school_name, parent_auth_key, teacher_auth_key)VALUES ('サンプル小学校', 'parent_key_example', 'teacher_key_example');
+
+INSERT INTO academic_levels (grade, section, school_id) VALUES ('1年', 'A組', 1);
+
+INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('五条　悟', 'ゴジョウ　サトル', '五条　由紀子','satoru@example.com', '011f55b984b0ea1e5e1d6a32df1ac1548fe24f8c20d85012b04c6fc5139abb42', 'student', '090-1234-5678', 1, 1);
+INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('夏油　傑', 'ゲトウ　スグル' NULL,'suguru@example.com', '54aaa3aced18c8f91fa47b375ea186680236a0359f80dcf483ad7b90b72f8d0d', 'teacher', '080-8765-4321', 1, 1);
+INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('家入　硝子', 'イエイリ　ショウコ', NULL, ,'syouko@example.com', 'ade7c829d3edab5cdb685fa383149fa867d0880136341ecfe1e1128dc9e2dfdd', 'teacher', '080-4321-8765', 1, 1);
+
+INSERT INTO groups (group_name, academic_level_id) VALUES ('グループA', 1);
+
+INSERT INTO user_groups (user_id, group_id) VALUES (1, 1), (2, 1), (3, 1);
+
+INSERT INTO messages (message, user_id, group_id) VALUES ('初めまして。', 1, 1);
