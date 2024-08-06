fddd9af4f129cfaee494e6d32946e79c8926ad71
Fuji <jifu7833@gmail.com>
2024-08-06 21:04:20 +0900

    [fix]init.sql修正

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -4,7 +4,7 @@ DROP USER 'testuser';
 
 CREATE USER 'testuser' IDENTIFIED BY 'testuser';
 CREATE DATABASE chimyapp;
-USE chatapp
+USE chimyapp;
 GRANT ALL PRIVILEGES ON chimyapp.* TO 'testuser';
 
 CREATE TABLE schools (
@@ -78,9 +78,10 @@ INSERT INTO schools (school_code, school_name, parent_auth_key, teacher_auth_key
 
 INSERT INTO academic_levels (grade, section, school_id) VALUES ('1年', 'A組', 1);
 
-INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('五条　悟', 'ゴジョウ　サトル', '五条　由紀子','satoru@example.com', '011f55b984b0ea1e5e1d6a32df1ac1548fe24f8c20d85012b04c6fc5139abb42', 'student', '090-1234-5678', 1, 1);
-INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('夏油　傑', 'ゲトウ　スグル' NULL,'suguru@example.com', '54aaa3aced18c8f91fa47b375ea186680236a0359f80dcf483ad7b90b72f8d0d', 'teacher', '080-8765-4321', 1, 1);
-INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)VALUES('家入　硝子', 'イエイリ　ショウコ', NULL, ,'syouko@example.com', 'ade7c829d3edab5cdb685fa383149fa867d0880136341ecfe1e1128dc9e2dfdd', 'teacher', '080-4321-8765', 1, 1);
+INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id)
+VALUES  ('五条　悟', 'ゴジョウ　サトル', '五条　由紀子', 'satoru@example.com', '011f55b984b0ea1e5e1d6a32df1ac1548fe24f8c20d85012b04c6fc5139abb42', 'student', '090-1234-5678', 1, 1),
+        ('夏油　傑', 'ゲトウ　スグル', NULL, 'suguru@example.com', '54aaa3aced18c8f91fa47b375ea186680236a0359f80dcf483ad7b90b72f8d0d', 'teacher', '080-8765-4321', 1, 1),
+        ('家入　硝子', 'イエイリ　ショウコ', NULL, 'syouko@example.com', 'ade7c829d3edab5cdb685fa383149fa867d0880136341ecfe1e1128dc9e2dfdd', 'teacher', '080-4321-8765', 1, 1);
 
 INSERT INTO groups (group_name, academic_level_id) VALUES ('グループA', 1);
 
