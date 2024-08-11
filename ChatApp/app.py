e88bdc634895942a1d2ccf6650ac1ad4c3da1d8e
take917 <1tktk2tk3@gmail.com>
2024-08-11 12:14:51 +0900

    homeページのサンプル例を作成

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash, abort
 from datetime import timedelta
+import calendar
+from datetime import datetime
 import hashlib
 import uuid
 import re
@@ -20,9 +22,36 @@ def login():
 def auth():
     return render_template('registration/auth.html')
 
-# @app.route('home')
-# def home():
-#     return
+
+@app.route('/home')
+def home():
+    # 現在の年月日を取得
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    today = now.day
+
+    #カレンダー作成
+    #
+    # 週の始まりを日曜にする
+    cal = calendar.Calendar(firstweekday=6)
+    month_days = cal.monthdayscalendar(year,month)
+
+    # チャットデータ
+    child_class = "5年3組"
+    teacher = "佐藤　道哉"
+    teacher_message = "本日もよろしくお願いします"
+    teacher_message_time = "7:46"
+    student_name = "田中　智樹"
+    group_name = "グループ"
+    group_message = "グループメッセージだよ〜"
+    group_message_time ="7:50 "
+
+
+
+    return render_template('registration/home.html',year=year,month=month,month_days=month_days,today=today,child_class=child_class,teacher=teacher,teacher_message=teacher_message,teacher_message_time=teacher_message_time,student_name=student_name,group_name=group_name,group_message=group_message,group_message_time=group_message_time)
+
+
 
 
 if __name__ == '__main__':
