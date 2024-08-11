18ffb49aa0e3ffe6f9b178d8ec877ee3e6f29a1e
Fuji <jifu7833@gmail.com>
2024-08-11 16:20:06 +0900

    [fix]models.pyの修正

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -16,12 +16,12 @@ class dbConnect:
             cur.close()      
 
 
-    def createUser(user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id):
+    def createUser(name_kanji_full, name_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id):
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
             sql = "INSERT INTO users (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
-            cur.execute(sql, (user_kanji_full, user_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id))
+            cur.execute(sql, (name_kanji_full, name_kana_full, parent_name, email, password, role, phone_number, academic_level_id, school_id))
             conn.commit()
         except Exception as e:
             print(f'{e} が発生してます') 
