e56b28b1f609185018457ad4f9c761f51d027e42
fugashi <syarrystory21@gmail.com>
2024-08-18 22:59:46 +0900

    registration内のhtmlと関係するlogin修正

--- ChatApp/static/js/login.js
+++ ChatApp/static/js/login.js
@@ -1,37 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  try {
-    setTimeout(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
       const formOverlay = document.querySelector('.form-overlay');
       if (formOverlay) {
-        formOverlay.classList.remove('hidden');
-        requestAnimationFrame(() => {
-          formOverlay.style.opacity = 1; // フェードイン効果を開始
-          formOverlay.style.transform = 'translateY(0)'; // 浮かび上がる効果を開始
-        });
+          formOverlay.style.opacity = 1; // フェードインを開始
       } else {
-        console.error('formOverlay not found');
+          console.error('formOverlay not found');
       }
-    }, 2000); // 2秒後にフォームを表示
-
-    // アニメーション終了時に再度繰り返さないようにする
-    const honhiLElement = document.querySelector('#honhiL');
-    if (honhiLElement) {
-      honhiLElement.addEventListener('animationend', function() {
-        this.style.animation = 'none';
-      });
-    } else {
-      console.error('#honhiL not found');
-    }
-
-    const leftElement = document.querySelector('.left');
-    if (leftElement) {
-      leftElement.addEventListener('animationend', function() {
-        this.style.animation = 'none';
-      });
-    } else {
-      console.error('.left not found');
-    }
-  } catch (error) {
-    console.error('Error during DOMContentLoaded:', error);
-  }
+  }, 2000); // 2秒後にフォームを表示
 });
